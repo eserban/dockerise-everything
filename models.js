255b8a2c9e73bd52927cd2bc75d659e917e29747
Ediard Serban <serbanedi.fr@gmail.com>
2022-02-20 18:08:01 +0100

    POST add books done

--- models.js
+++ models.js
@@ -1,10 +1,14 @@
-function book(isbn, title, author, overview, picture, red_count){
+function bookModel(isbn, title, author, overview, picture, read_count){
     return {
         "isbn": isbn,
         "title": title,
         "author": author,
         "overview": overview,
         "picture": picture,
-        "red_count": red_count
+        "red_count": read_count
     };
+}
+
+module.exports = {
+    bookModel
 }
--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const {bookModel} = require("../models.js");
 let router = express.Router();
 
 require('dotenv').config();
@@ -17,15 +18,94 @@ const client = new MongoClient(uri, {
 
 (async () => {
     await client.connect();
+    const booksCollection = await client.db(dbName).collection("books");
 
 
     router.get('/books', async (req, res) => {
 
-        const booksCollection = await client.db(dbName).collection("books");
+        
         const books = await booksCollection.find().toArray();
 
         res.status(200).send(books);
     });
+
+
+    router.post('/books', async(req, res) => {
+
+        
+
+        const isbn = req.body.isbn ?? null;
+        const title = req.body.title ?? null;
+        const author = req.body.author ?? null;
+        const overview = req.body.overview ?? null;
+        const picture = req.body.picture ?? null;
+        const readCount = req.body.read_count ?? 1;
+
+        console.log(typeof isbn);
+        console.log(typeof title);
+        console.log(typeof author);
+        console.log(typeof overview);
+        console.log(typeof picture);
+        console.log(typeof readCount);
+
+        let success = true;
+        let code = 200;
+        let errors = [];
+        let response = [];
+
+        let book = await booksCollection.find({"isbn": isbn}).toArray();
+
+        if(!isbn || !title || !author) {
+            success = false;
+            code = 422;
+            errors.push("Les éléments obligatoires ne sont pas fournis");
+        }
+
+        if(isbn.length > 13 || typeof isbn !== "string"){
+            success = false;
+            code = 422;
+            errors.push("L'isbn doit faire 13 caractères max");
+        }
+
+        if(title.length > 200 || typeof title !== "string") {
+            success = false;
+            code = 422;
+            errors.push("Le titre ne doit pas dépasser les 200 caractères");
+        }
+
+        if(title.length > 150 || typeof title !== "string") {
+            success = false;
+            code = 422;
+            errors.push("Le nom de l'autheur ne doit pas dépasser les 150 caractères");
+        }
+
+        if(overview && (overview.length > 1500 || typeof overview !== "string")){
+            success = false;
+            code = 422;
+            errors.push("La 4ème de couverture ne doit pas depasser les 1500 caractères");
+        }
+
+        if(success && book.lengh > 0){
+            success = false;
+            code = 422;
+            errors.push("Ce livre existe deja");
+        }
+
+        if(success) {
+            let finalBook = bookModel(isbn, title, author, overview, picture, readCount)
+            await booksCollection.insertOne(finalBook);
+            response = await booksCollection.findOne(finalBook);
+        }
+
+       let data = {
+           "errors": errors,
+           "book": response
+       }
+
+        res.status(code).send(data);
+
+    });
+
 
 })();
 
